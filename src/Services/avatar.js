189f4a598bc7e37009670c482f58f07278b14a1c
Fiona Ochs <fiona.oaks@gmail.com>
2020-05-06 17:22:17 -0700

    test

--- src/Services/avatar.js
+++ src/Services/avatar.js
@@ -1,5 +1,17 @@
+// export const fetchCharacters = () => {
+//   return fetch('https://last-airbender-api.herokuapp.com/api/v1/characters')
+//     .then(res => res.json())
+//     .then(json => json.map(character => ({
+//       id: character._id,
+//       name: character.name,
+//       allies: character.allies,
+//       enemies: character.enemies,
+//       affiliation: character.affiliation,
+//       image: character.photoUrl
+//     })));
+// };
 export const fetchCharacters = () => {
-  return fetch('https://last-airbender-api.herokuapp.com/api/v1/characters')
+  return fetch('https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=${5}?page=${1}')
     .then(res => res.json())
     .then(json => json.map(character => ({
       id: character._id,
